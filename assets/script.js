5281e015253a358a2eb756b248141055eaae8761
Edson Petry <edsonlbpetry@gmail.com>
2023-03-15 22:28:57 -0400

    Formatted code + changed questions array

--- assets/script.js
+++ assets/script.js
@@ -1,133 +1,92 @@
+// Variables
+const hidePage = document.querySelector(".hidePage");
+const startButton = document.querySelector("#startQuizBtn");
+const showQuestions = document.querySelector(".questions");
+const question1 = document.querySelector("#question1");
+
+const points = "";
+let timer = document.querySelector(".timer");
+var count = 300;
+
+// Event listeners
+startButton.addEventListener("click", startQuiz);
+
+// Functions to complete tasks
+function startQuiz() {
+  // Hides home page
+  hidePage.style.display = "none";
+  // Shows question 1
+  showQuestions.style.display = "grid";
+  startCountdown();
+}
+
+
+
+function startCountdown() {
+  timer = setInterval(function () {
+    count--;
+    timer.innerText = "Remaining time: " + count;
+
+    if (count === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
 // QUESTIONS
 const questions = [
-    {
-        question: 'What language should you use to build the structure for your website?',
-        optionA: 'HTML',
-        optionB: 'JavaScript',
-        optionC: 'CSS',
-        optionD: 'Python',
-        correctAnswer: 'HTML'
-    },
+  {
+    question:
+      "What language should you use to build the structure for your website?",
+    answers: [
+      { text: "HTML", correct: true },
+      { text: "JavaScript", correct: false },
+      { text: "CSS", correct: false },
+      { text: "Python", correct: false },
+    ],
+  },
 
-    {
-        question: 'How do you select an ID in your HTML from your stylesheet?',
-        optionA: '#',
-        optionB: '.',
-        optionC: '.getElementByID',
-        optionD: ';',
-        correctAnswer: '#'
-    },
+  {
+    question: "How do you select an ID in your HTML from your stylesheet?",
+    answers: [
+      { text: "#", correct: true },
+      { text: ".", correct: false },
+      { text: ".getElementByID", correct: false },
+      { text: ";", correct: false },
+    ],
+  },
 
-    {
-        question: 'Which of the follow is a valid HTML tag?',
-        optionA: 'h1',
-        optionB: '<>',
-        optionC: '<h1>',
-        optionD: '>h1,',
-        correctAnswer: '<h1>'
-    },
+  {
+    question: "Which of the follow is a valid HTML tag?",
+    answers: [
+      { text: "h1", correct: false },
+      { text: "<>", correct: false },
+      { text: "<h1>", correct: true },
+      { text: ">h1,", correct: false },
+    ],
+  },
 
-    {
-        question: 'Which of the following is an example of metadata about a webpage?',
-        optionA: 'The title of the webpage',
-        optionB: 'The body of the webpage',
-        optionC: 'An <h1> tag on the webpage',
-        optionD: 'All of the above',
-        correctAnswer: 'The title of the webpage'
-    },
+  {
+    question:
+      "Which of the following is an example of metadata about a webpage?",
+    answers: [
+      { text: "The title of the webpage", correct: true },
+      { text: "The body of the webpage", correct: false },
+      { text: "An <h1> tag on the webpage", correct: false },
+      { text: "All of the above", correct: false },
+    ],
+  },
 
-    {
-        question: 'What does the <br> tag do?',
-        optionA: 'Creates a line break on the webpage',
-        optionB: 'Creates a horizontal line on the webpage',
-        optionC: 'Bolds text',
-        optionD: 'None of the above',
-        correctAnswer: 'Creates a line break on the webpage'
-    },
-
-    {
-        question: 'Inside which HTML element do we link our JavaScript file?',
-        optionA: '<javascript>',
-        optionB: '<js>',
-        optionC: '<link>',
-        optionD: '<script>',
-        correctAnswer: '<script>'
-    },
-
-    {
-        question: 'Which of the follow is not a data type in JavaScript?',
-        optionA: 'objects',
-        optionB: 'strings',
-        optionC: 'numbers',
-        optionD: 'variables',
-        correctAnswer: 'variable'
-    },
-
-    { 
-        question: 'Is Java the same as JavaScript',
-        optionA: 'No',
-        optionB: 'Yes',
-        optionC: 'What',
-        optionD: 'They are the same, one is just used for mobile apps.',
-        correctAnswer: 'Yes'
-    },
-
-    {
-        question: 'How do you call a function in JavaScript?',
-        optionA: 'call myFunction()',
-        optionB: 'myFunction()',
-        optionC: 'call function myFunction',
-        optionD: 'call.myFunction()',
-        correctAnswer: 'myFunction()'
-    },
-
-    {
-        question: 'How can you make comments within JavaScript?',
-        optionA: '//This is a comment',
-        optionB: '<!--This is a comment-->',
-        optionC: '#This is a commment',
-        optionD: '$$This is a comment$$',
-        correctAnswer: '//This is a comment'
-    }
-
-]
-
-const hidePage = document.querySelector('.hidePage')
-const startButton = document.querySelector('#startQuizBtn')
-const showQuestions = document.querySelector('.questions')
-const question1 = document.querySelector('#question1')
-const optionA = document.querySelector('.optionA')
-const optionB = document.querySelector('.optionB')
-const optionC = document.querySelector('.optionC')
-const optionD = document.querySelector('.optionD')
-
-startButton.addEventListener('click', startQuiz)
-
-function startQuiz() {
-    hidePage.style.display = 'none'
-    showQuestions.style.display = 'block'
-    showQuiz()
-}
-
-optionA.addEventListener('click', nextQuestion)
-optionB.addEventListener('click', nextQuestion)
-optionC.addEventListener('click', nextQuestion)
-optionD.addEventListener('click', nextQuestion)
-
-function showQuiz() {
-    question1.textContent = questions[0].question
-    optionA.textContent = questions[0].optionA
-    optionB.textContent = questions[0].optionB
-    optionC.textContent = questions[0].optionC
-    optionD.textContent = questions[0].optionD
-}
-
-function nextQuestion () {
-    console.log(questions[0].correctAnswer)
-    console.log(this.innerHTML)
-    if //create if statement to check correct answer
-}
-
+  {
+    question: "What does the <br> tag do?",
+    answers: [
+      { text: "Creates a line break on the webpage", correct: true },
+      { text: "Creates a horizontal line on the webpage", correct: false },
+      { text: "Bolds text", correct: false },
+      { text: "None of the above", correct: false },
+    ],
+  },
+];
 
 /* 
 GIVEN I am taking a code quiz
