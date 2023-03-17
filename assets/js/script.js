199e643495b30c86aa5709994e3c3560bc65d961
Edson Petry <edsonlbpetry@gmail.com>
2023-03-16 23:32:18 -0400

    CSS AND HTML DONE, OUTLINED JAVASCRIPT!!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 // GIVEN I am taking a code quiz
 
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
+/* Add event listener for startBtn so when it is clicked it runs a function called
+startQuiz */
+
+/* startQuiz function will add hide class on our start-page div and remove hide class on our
+showQuiz div */
+    // startQuiz function will run our startTimer function and run displayQuestion function
 
 
 
-// WHEN I answer a question
-// THEN I am presented with another question
+/* Add event listener for option buttons, when option butten is clicked run checkAnswer
+function, if option is wrong, display quiz div background red + subtract time from startTimer
+if option is correct, display quiz div background green */
+// after option is selected display next button
+// set an event listener for next button
+// WHEN next button is clicked next answer should pop up
 
+//IF all answer are answer || the timer reaches 0
+// THEN a highscore is displayed with the option to enter your name 
+// If player enters name, highscore and name get saved to a Local Database on  the browser
 
-
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-
-
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-
-
-// WHEN the game is over
-// THEN I can save my initials and score
