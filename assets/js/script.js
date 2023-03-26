964dbd15b30c0d347e64c467ea3c5da4f231d4c4
Edson Petry <edsonlbpetry@gmail.com>
2023-03-26 17:19:28 -0400

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,29 @@ function startQuiz() {
     startTimer()
 }
 
-setInterval(startTimer, 1000)
 
-function startTimer() {
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
 }
+
+window.onload = function () {
+    let fiveMinutes = 60 * 10,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
 
 
 /* startQuiz function will add hide class on our start-page div and remove hide class on our
