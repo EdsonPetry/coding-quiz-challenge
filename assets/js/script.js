0adf71494c29e9a69025b8bddbb7cad2c5525359
Edson Petry <edsonlbpetry@gmail.com>
2023-03-30 09:59:49 -0400

    fixed timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,29 @@
-// GIVEN I am taking a code quiz
+// Global variables
 let startBtn = document.querySelector("#start-btn");
 let startPage = document.querySelector(".start-page");
 let quizPage = document.querySelector(".quiz-div");
 let optionBtn = document.querySelector(".answerBtn");
 let endPage = document.querySelector(".end-game");
-var currentQuestionIndex = 0;
+let currentQuestionIndex = 0;
 let choicesEl = document.querySelector("#choices");
 let timer;
-/* Add event listener for startBtn so when it is clicked it runs a function called
-startQuiz */
+
 startBtn.addEventListener("click", startQuiz);
 // Starts quiz and timer
 function startQuiz() {
   startPage.classList.add("hide");
   quizPage.classList.remove("hide");
-  // startTimer();
+  countdown();
   displayQuestion();
 }
 
 function displayQuestion() {
-  var currentQuestion = questions[currentQuestionIndex];
-  var titleEl = document.querySelector("#question-display");
+  let currentQuestion = questions[currentQuestionIndex];
+  let titleEl = document.querySelector("#question-display");
   titleEl.textContent = currentQuestion.question;
-  for (var i = 0; i < currentQuestion.choices.length; i++) {
-    var choice = currentQuestion.choices[i];
-    var choiceBtn = document.createElement("button");
+  for (let i = 0; i < currentQuestion.choices.length; i++) {
+    let choice = currentQuestion.choices[i];
+    let choiceBtn = document.createElement("button");
     choiceBtn.setAttribute("class", "class");
     choiceBtn.setAttribute("value", choice);
     choiceBtn.textContent = choice;
@@ -32,27 +31,23 @@ function displayQuestion() {
   }
 }
 
+choicesEl.addEventListener("click", questionClick);
 function questionClick(event) {
-  var buttenEl = event.target;
-  if (!buttenEl.matches('.choice')) {
+  let buttenEl = event.target;
+  if (!buttenEl.matches(".choice")) {
     return;
-  } 
+  }
   if (buttenEl.value !== questions[currentQuestionIndex].answer) {
-    timer = timer - 10
-    // Make background color of the box red
+    timer = timer - 10;
+    quizPage.setAttribute("style", "background-color:red");
   } else {
-    // Make background color of the box green
+    quizPage.setAttribute("style", "background-color:green");
   }
 }
-// Listens for click on button and checks if correct = true, if correct does not = true then subtract time from timer
-// optionBtn.addEventListener('click', corrrectAnswer)
-// correctAnswer() {
-
-// }
 
 // Timer and countsdown, if timer equals 0:00 then display end game screen
 function startTimer(duration, display) {
-  timer = duration,
+  let timer = duration,
     minutes,
     seconds;
   setInterval(function () {
@@ -78,23 +73,8 @@ function timesUp() {
   endPage.classList.remove("hide");
 }
 
-window.onload = function () {
+function countdown() {
   let fiveMinutes = 60 * 5,
     display = document.querySelector("#time");
   startTimer(fiveMinutes, display);
-};
-
-/* startQuiz function will add hide class on our start-page div and remove hide class on our
-showQuiz div */
-// startQuiz function will run our startTimer function and run displayQuestion function
-
-/* Add event listener for option buttons, when option butten is clicked run checkAnswer
-function, if option is wrong, display quiz div background red + subtract time from startTimer
-if option is correct, display quiz div background green */
-// after option is selected display next button
-// set an event listener for next button
-// WHEN next button is clicked next question  should pop up
-
-//IF all answer are answer || the timer reaches 0
-// THEN a highscore is displayed with the option to enter your name
-// If player enters name, highscore and name get saved to a Local Database on  the browser
+}
